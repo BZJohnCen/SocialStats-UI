c928e04948e6ff1a08834166274bdf3b12404c12
John Cen <john.cen397@gmail.com>
2019-03-12 19:13:23 -0400

    Introduces the route lists and authenticated private routes, with an authentication method.

---
+++ social-stats/src/routes/RouteList.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import Login from '../components/Login';
+import Home from '../views/Home';
+// import SideBar from '../components/SideBar';
+import Test from '../Test';
+
+const RouteList = () => (
+  <Switch>
+    <Route path='/login' component={Login} />
+    {/*<Route path='/' component={SideBar} />*/}
+    <PrivateRoute exact path='/' component={Home} />
+    <Route path='/test' component={Test} />
+  </Switch>
+)
+
+const isAuthenticated = () => {
+  //insert our client-side auth
+}
+
+const PrivateRoute = ({ component: Component, path, otherProps }) => (
+  <Route
+    { ...{ path } }
+    render = { (props) => (
+      isAuthenticated() ? ( <Component {...props} {...otherProps} /> ) :
+        ( <Redirect push to = { {pathname: '/login', state: { from: props.location }}}/>)
+      )
+    }
+  />
+);
+
+export default RouteList;
