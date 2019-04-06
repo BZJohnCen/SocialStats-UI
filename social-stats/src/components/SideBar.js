f1b5650694391ed2901c88ac5f96605438bd2108
John Cen <john.cen397@gmail.com>
2019-04-05 23:28:30 -0400

    Updates SideBar component.

--- social-stats/src/components/SideBar.js
+++ social-stats/src/components/SideBar.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import styled from '@emotion/styled';
-import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, Button } from 'reactstrap';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, Button } from 'reactstrap';
 import { withRouter } from 'react-router';
+import { NavLink } from 'react-router-dom';
 
 const SidebarDiv = styled.div`
   height: 100%;
@@ -13,13 +14,26 @@ const SidebarDiv = styled.div`
 const Sidebar = styled(Navbar)`
   height: auto;
 `;
-const Navlink = styled(NavLink)`
+const Link = styled(NavLink)`
   justify-content: center;
   align-items: center;
+  margin: 0.3em 0em 0.3em 0em;
+
+  padding: 0.3em 1em;
+  color: #E0E0E0;
+
+  :hover{
+    text-decoration: none;
+    color: #fff;
+  }
 `;
 const LogoutButton = styled(Button)`
   margin-top: 2em;
-  width: 70%;
+  background-color: transparent;
+`;
+const SideBarLinks = styled.div`
+  display: flex;
+  flex-direction: column;
 `;
 
 class SideBar extends Component{
@@ -55,25 +69,27 @@ class SideBar extends Component{
     return (
       <SidebarDiv id="sideBar" className={visible}>
         <Sidebar color="faded" dark className='bg-dark'>
-          <NavbarBrand href="/" className="mr-auto">SocialStats</NavbarBrand>
+          <NavLink to="/" style={{ color: '#fff' }}><NavbarBrand className="mr-auto">SocialStats</NavbarBrand></NavLink>
           <NavbarToggler id="navToggle" onClick={this.toggleSideNav} className="mr-2" />
           <Collapse isOpen={this.state.isOpen} navbar>
             <div style={{ paddingTop: '1em', paddingBottom: '1em'}}>
-            <Nav navbar>
-              <NavItem>
-                <Navlink href="/current">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="#fff" width="24" height="24" viewBox="0 0 24 24"><path d="M24 3.875l-6 1.221 1.716 1.708-5.351 5.358-3.001-3.002-7.336 7.242 1.41 1.418 5.922-5.834 2.991 2.993 6.781-6.762 1.667 1.66 1.201-6.002zm0 16.125v2h-24v-20h2v18h22z"/></svg>
-                  Current
-                </Navlink>
-              </NavItem>
-              <NavItem>
-                <Navlink href="/signup">Signup</Navlink>
-              </NavItem>
-              <NavItem>
-                <Navlink href="/test">Test</Navlink>
-              </NavItem>
-              <LogoutButton onClick={this.handleLogout}>Log out</LogoutButton>
-            </Nav>
+              <Nav navbar>
+                <SideBarLinks>
+                  <Link to="/dashtest" activeStyle={{ padding: '0.3em 1em 0.3em 1em', textDecoration: 'none', color: '#F2F2F2', border: '0.04em solid #617DA3', borderRadius: '2em', backgroundColor: '#617DA3' }}>
+                    <svg xmlns="http://www.w3.org/2000/svg" style={{ marginRight: '0.8em' }} fill="#fff" width="24" height="24" viewBox="0 0 24 24"><path d="M24 3.875l-6 1.221 1.716 1.708-5.351 5.358-3.001-3.002-7.336 7.242 1.41 1.418 5.922-5.834 2.991 2.993 6.781-6.762 1.667 1.66 1.201-6.002zm0 16.125v2h-24v-20h2v18h22z"/></svg>
+                    Twitter
+                  </Link>
+                  <Link to="/home" activeStyle={{ padding: '0.3em 1em 0.3em 1em', textDecoration: 'none', color: '#F2F2F2', border: '0.04em solid #617DA3', borderRadius: '2em', backgroundColor: '#617DA3' }}>
+                    <svg xmlns="http://www.w3.org/2000/svg" style={{ marginRight: '0.8em' }} fill="#fff" width="24" height="24" viewBox="0 0 24 24"><path d="M24 3.875l-6 1.221 1.716 1.708-5.351 5.358-3.001-3.002-7.336 7.242 1.41 1.418 5.922-5.834 2.991 2.993 6.781-6.762 1.667 1.66 1.201-6.002zm0 16.125v2h-24v-20h2v18h22z"/></svg>
+                    Callback
+                  </Link>
+                  <Link to="/test" activeStyle={{ padding: '0.3em 1em 0.3em 1em', textDecoration: 'none', color: '#F2F2F2', border: '0.04em solid #617DA3', borderRadius: '2em', backgroundColor: '#617DA3' }}>
+                    <svg xmlns="http://www.w3.org/2000/svg" style={{ marginRight: '0.8em' }} fill="#fff" width="24" height="24" viewBox="0 0 24 24"><path d="M24 3.875l-6 1.221 1.716 1.708-5.351 5.358-3.001-3.002-7.336 7.242 1.41 1.418 5.922-5.834 2.991 2.993 6.781-6.762 1.667 1.66 1.201-6.002zm0 16.125v2h-24v-20h2v18h22z"/></svg>
+                    Test
+                  </Link>
+                </SideBarLinks>
+                <LogoutButton onClick={this.handleLogout}>Log out</LogoutButton>
+              </Nav>
             </div>
           </Collapse>
         </Sidebar>
